3a48c50597dece1afc578f2041b177cc65c46433
Tianyuan Jia <t22jia@uwaterloo.ca>
2026-08-15 22:05:12 +0800

    Add BH1750 light sensing with shared I2C bus

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <BH1750.h>
 
 Adafruit_BME280 bme;
+BH1750 lightMeter;
 
 bool bmeReady = false;
+bool bh1750Ready = false;
 
 const int SDA_PIN = 8;
 const int SCL_PIN = 9;
@@ -33,28 +36,51 @@ void setup()
   {
     Serial.println("BME280 not detected.");
   }
+
+  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire))
+  {
+    bh1750Ready = true;
+    Serial.println("BH1750 detected at address 0x23.");
+  }
+  else
+  {
+    Serial.println("BH1750 not detected.");
+  }
 }
 
 void loop()
 {
-  if (!bmeReady)
+  if (bmeReady)
+  {
+    Serial.print("Temperature: ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" C");
+
+    Serial.print("Humidity: ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.print("Pressure: ");
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+  }
+  else
   {
     Serial.println("BME280 unavailable.");
-    delay(2000);
-    return;
   }
 
-  Serial.print("Temperature: ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" C");
+  if (bh1750Ready)
+  {
+    float lux = lightMeter.readLightLevel();
 
-  Serial.print("Humidity: ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
-
-  Serial.print("Pressure: ");
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println(" hPa");
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+  }
+  else
+  {
+    Serial.println("BH1750 unavailable.");
+  }
 
   Serial.println();
   delay(2000);
