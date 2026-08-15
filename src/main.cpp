1a26d9c4b0910b5aba0498b7328a7c466201642a
Tianyuan Jia <t22jia@uwaterloo.ca>
2026-08-15 10:18:11 +0800

    Refine BME280 baseline with explicit I2C pin mapping

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,42 @@
 #include <Adafruit_BME280.h>
 
 Adafruit_BME280 bme;
+
 bool bmeReady = false;
 
-void setup() {
+const int SDA_PIN = 8;
+const int SCL_PIN = 9;
+
+void setup()
+{
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("ESP32 Environment Monitor");
+  Serial.println("ESP32 Study Environment Monitor");
 
-  Wire.begin();
+  Wire.begin(SDA_PIN, SCL_PIN);
 
-  if (bme.begin(0x76, &Wire)) {
+  if (bme.begin(0x76, &Wire))
+  {
     bmeReady = true;
     Serial.println("BME280 detected at address 0x76.");
-  } else if (bme.begin(0x77, &Wire)) {
+  }
+  else if (bme.begin(0x77, &Wire))
+  {
     bmeReady = true;
     Serial.println("BME280 detected at address 0x77.");
-  } else {
+  }
+  else
+  {
     Serial.println("BME280 not detected.");
-    Serial.println("Connect the sensor, then restart the ESP32.");
   }
 }
 
-void loop() {
-  if (!bmeReady) {
-    Serial.println("Waiting for BME280...");
+void loop()
+{
+  if (!bmeReady)
+  {
+    Serial.println("BME280 unavailable.");
     delay(2000);
     return;
   }
